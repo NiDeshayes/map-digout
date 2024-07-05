1dc8e41b0e1441f4442296e831f6b8b5a371cbac
ovatio <dnicolas120@gmail.com>
2024-07-05 17:56:29 +0200

    mise à jour

--- contact.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-contact_bp = Blueprint('contact', __name__, url_prefix='/contact')
-
-@contact_bp.route('/')
-def contact():
-    return render_template('contact.html')
